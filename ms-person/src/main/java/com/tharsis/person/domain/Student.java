33fd5308fe288aaf6f12e7e6cc3847cd72cbb7a0
Christian Loza <Christianlzap@gmail.com>
2020-04-10 14:58:12 -0500

    Fix NamedQuery Student

--- ms-person/src/main/java/com/tharsis/person/domain/Student.java
+++ ms-person/src/main/java/com/tharsis/person/domain/Student.java
@@ -32,7 +32,7 @@ import lombok.Setter;
     @NamedQuery(name = "Student.findAllById", query = "SELECT p.idperson, p.dni, p.name, p.lastname, p.phone, p.email  FROM Student s JOIN s.person p  WHERE p.status = 1 AND p.idperson = :idperson"),
     @NamedQuery(name = "Student.findByIdstudent", query = "SELECT NEW com.tharsis.person.domain.Student(p.idperson, p.dni, p.name, p.lastname, p.phone, p.email, p.status) FROM Student s JOIN s.person p WHERE p.idperson =:idperson AND p.status = 1"),
     @NamedQuery(name = "Student.findByDnistudent", query = "SELECT NEW  com.tharsis.person.domain.Student(s.idnfc,p.idperson, p.dni, p.name, p.lastname, p.phone, p.email)  FROM Student s JOIN s.person p WHERE p.dni = :dni AND p.status = 1"),
-    @NamedQuery(name = "Student.finIdByNfc", query = "SELECT NEW  com.tharsis.person.domain.Student(p.idperson) FROM Student s JOIN s.person p WHERE s.idnfc = :idnfc AND s.person.status = 1")
+    @NamedQuery(name = "Student.finIdByNfc", query = "SELECT p.idperson FROM Student s JOIN s.person p WHERE s.idnfc = :idnfc AND s.person.status = 1")
 })
 
 @Getter
@@ -89,11 +89,5 @@ public class Student implements Serializable {
         this.getPerson().setLastname(lastName);
         this.getPerson().setPhone(phone);
         this.getPerson().setEmail(email);
-    }
-
-    //Student.finIdByNfc
-    public Student(Integer idperson) {
-        this.setPerson(new Person());
-        this.getPerson().setIdperson(idperson);
     }
 }
